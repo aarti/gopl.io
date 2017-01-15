3d53fad0ee34d81b125b009f35dbbe85e2e0f08d
Aarti Parikh <aartiwithcode@gmail.com>
2017-01-15 14:53:40 -0800

    Exercise 9.1: Withdraw

--- ch9/bank1/bank.go
+++ ch9/bank1/bank.go
@@ -7,11 +7,23 @@
 // Package bank provides a concurrency-safe bank with one account.
 package bank
 
-var deposits = make(chan int) // send amount to deposit
-var balances = make(chan int) // receive balance
+var deposits = make(chan int)                      // send amount to deposit
+var balances = make(chan int)                      // receive balance
+var withdrawRequests = make(chan *WithdrawRequest) // send withdraw request
+
+type WithdrawRequest struct {
+	amount     int
+	resultChan chan bool
+}
 
 func Deposit(amount int) { deposits <- amount }
 func Balance() int       { return <-balances }
+func Withdraw(amount int) bool {
+	wr := &WithdrawRequest{amount, make(chan bool)}
+	// Send Request
+	withdrawRequests <- wr
+	return <-wr.resultChan
+}
 
 func teller() {
 	var balance int // balance is confined to teller goroutine
@@ -20,6 +32,13 @@ func teller() {
 		case amount := <-deposits:
 			balance += amount
 		case balances <- balance:
+		case wr := <-withdrawRequests:
+			if balance > wr.amount {
+				balance -= wr.amount
+				wr.resultChan <- true
+			} else {
+				wr.resultChan <- false
+			}
 		}
 	}
 }
--- ch9/bank1/bank_test.go
+++ ch9/bank1/bank_test.go
@@ -16,21 +16,46 @@ func TestBank(t *testing.T) {
 	// Alice
 	go func() {
 		bank.Deposit(200)
-		fmt.Println("=", bank.Balance())
+		fmt.Println("Deposit 200, Bank Balance=", bank.Balance())
 		done <- struct{}{}
 	}()
 
 	// Bob
 	go func() {
 		bank.Deposit(100)
+		fmt.Println("Deposit 100, Bank Balance=", bank.Balance())
 		done <- struct{}{}
 	}()
 
-	// Wait for both transactions.
+	// John
+	go func() {
+
+		ok := bank.Withdraw(350)
+		if ok {
+			t.Errorf("Expected Withdraw of 350 to be %v", ok)
+		}
+		fmt.Println("Withdraw 350, Bank Balance=", bank.Balance())
+		done <- struct{}{}
+	}()
+
+	// Jane
+	go func() {
+		ok := bank.Withdraw(50)
+		if !ok {
+			t.Errorf("Expected Withdraw of 50 to be %v", ok)
+		}
+		fmt.Println("Withdraw 50, Bank Balance=", bank.Balance())
+		done <- struct{}{}
+	}()
+
+	// Wait for all transactions.
+	<-done
+	<-done
 	<-done
 	<-done
 
-	if got, want := bank.Balance(), 300; got != want {
+	if got, want := bank.Balance(), 250; got != want {
 		t.Errorf("Balance = %d, want %d", got, want)
 	}
+	fmt.Println("Final Bank Balance=", bank.Balance())
 }
